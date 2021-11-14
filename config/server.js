446cf8e3226b9ea571e1447d6ad50d4c685948a2
Mxkert <mikehendriks94@gmail.com>
2021-11-14 12:57:54 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac69e020f568cbe5f08ba19675145f83'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '605c5774-3efc-4503-9fdb-6b0fdd99be9a'
+};
